9ca94bf5a47b27c6719715563e69df7c76435425
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-20 21:20:53 -0300

    Delete user test

--- src/UserControllerTests.js
+++ src/UserControllerTests.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const signale = require('signale');
 
 
-const TOKEN  = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImlhdCI6MTUzMjEyMzQzMX0.XTeP5abpQspc60_6D-cF7Pt5zFVBWbehA0iXqccDkS4'
+const TOKEN  = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIsImlhdCI6MTUzMjEzMTk3OX0.1AN0Lycywo_wFSaPMvOjLXaMXSf8_PFsHTwLjjJ7fTc'
 axios.defaults.headers.common['Authorization'] = TOKEN;
 
     async function test(){
@@ -10,7 +10,7 @@ axios.defaults.headers.common['Authorization'] = TOKEN;
     /*
                 Teste show user
     */
-    await axios.get('http://localhost:3333/users/1',{
+    await axios.get('http://localhost:3333/users/2',{
         'Access-Control-Allow-Origin': "*"
     }).then(function(response){
         const data = response.data.user[0];
@@ -21,7 +21,24 @@ axios.defaults.headers.common['Authorization'] = TOKEN;
         }
     }).catch(function(error){
         signale.error('Show user falhou ! - ' + error.message);
-    })
+    });
+
+
+
+    /*
+                Teste Delete user
+    */
+   await axios.delete('http://localhost:3333/users/2/delete',{
+        'Access-Control-Allow-Origin': "*"
+    }).then(function(response){
+        if(response.status == 200 && response.data.message){
+            signale.success('Delete user bem sucedido !');
+        }else{
+            signale.error('Delete user falhou !');
+        }
+    }).catch(function(error){
+        signale.error('Delete user falhou ! - ' + error.message);
+    });
 }
 
 test();
