8fcafacca487b8fe48f11a95da11e2cfd71c8f0a
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-22 00:23:13 -0300

    aaaaaaaaaaaaaaa

--- src/AuthControllerTests.js
+++ src/AuthControllerTests.js
@@ -1,49 +1,56 @@
 const axios = require('axios');
 const signale = require('signale');
 
-    async function test(){
+    /*
+        TESTE DE REGISTRO DE USUÁRIO
+    */
+    async function register(){
+       axios.post('http://localhost:3333/register', {
+          username: 'mateus12',
+          email: 'mateus@hotmail.com.br',
+          password: '1234'
+      },
+      {
+          'Access-Control-Allow-Origin': "*"
+      }).then(async function(response){
+          console.log(response.status);
+          if(response.status == 201){
+              await signale.success('Registro bem sucedido !');
+              return true;
+          }else{
+              signale.error('Registro falhou !');
+              return false;
+          }
+      }).catch(function(error){
+          signale.error('Registro falhou ! - ' + error.message);
+          return false;
+      });
+    }
 
     /*
-                TESTE DE REGISTRO DE USUÁRIO
+        TESTE DE AUTENTICAÇÃO DE USUÁRIO
     */
-    await axios.post('http://localhost:3333/register', {
-        username: 'mateus12',
-        email: 'mateus@hotmail.com.br',
-        password: '1234'
-    },
-    {
-        'Access-Control-Allow-Origin': "*"
-    }).then(function(response){
-        console.log(response.status);
-        if(response.status == 201){
-            signale.success('Registro bem sucedido !');
-        }else{
-            signale.error('Registro falhou !')
-        }
-    }).catch(function(error){
-        signale.error('Registro falhou ! - ' + error.message);
-    })
+    async function auth(){
+       axios.post('http://localhost:3333/auth', {
+          email: 'mateus@hotmail.com.br',
+          password: '1234'
+      },
+      {
+      'Access-Control-Allow-Origin': "*"
+      }).then( function (response) {
+          if(response.status == 200 && response.data.token && response.data.type){
+              signale.success('Autenticação bem sucedida !');
+              console.log('Token de acesso: ' + response.data.token);
+              return true;
+          }else{
+              signale.error('Autenticação falhou !');
+              return false;
+          }
+      }).catch(function (error) {
+          signale.error('Autenticação falhou - ' + error.message);
+          return false;
+      });
+    }
 
-    /*
-            TESTE DE AUTENTICAÇÃO DE USUÁRIO
-    */
-    await axios.post('http://localhost:3333/auth', {
-        email: 'mateus@hotmail.com.br',
-        password: '1234'
-    },
-    {
-    'Access-Control-Allow-Origin': "*"
-    }).then(function (response) {
-        if(response.status == 200 && response.data.token && response.data.type){
-            signale.success('Autenticação bem sucedida !');
-            console.log('Token de acesso: ' + response.data.token);
-        }else{
-            signale.error('Autenticação falhou !');
-        }
-    }).catch(function (error) {
-        signale.error('Autenticação falhou - ' + error.message);
-    });
-}
-
-
-test();
+register();
+auth();
--- src/TypeControllerTests.js
+++ src/TypeControllerTests.js
@@ -4,12 +4,12 @@ const signale = require('signale');
 const TOKEN  = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImlhdCI6MTUzMjIyOTM0M30.oiuiH00UjRNvewHIhH3lWEKLYf1mdqe6-DVDw-GFBrA'
 axios.defaults.headers.common['Authorization'] = TOKEN;
 
-
     /*
                 Teste show user
     */
     async show(){
-      await axios.get('http://localhost:3333/users/2',{
+
+      await axios.get('localhost:3333/logs/types/store',{
           'Access-Control-Allow-Origin': "*"
       }).then(function(response){
           const data = response.data.user[0];
@@ -21,24 +21,24 @@ axios.defaults.headers.common['Authorization'] = TOKEN;
       }).catch(function(error){
           signale.error('Show user falhou ! - ' + error.message);
       });
+
     }
-
-
-
 
     /*
                 Teste Delete user
     */
    async delete(){
-     await axios.delete('http://localhost:3333/users/2/delete',{
-          'Access-Control-Allow-Origin': "*"
-      }).then(function(response){
-          if(response.status == 200 && response.data.message){
-              signale.success('Delete user bem sucedido !');
-          }else{
-              signale.error('Delete user falhou !');
-          }
-      }).catch(function(error){
-          signale.error('Delete user falhou ! - ' + error.message);
-      });
-   }
+
+   await axios.delete('http://localhost:3333/users/2/delete',{
+        'Access-Control-Allow-Origin': "*"
+    }).then(function(response){
+        if(response.status == 200 && response.data.message){
+            signale.success('Delete user bem sucedido !');
+        }else{
+            signale.error('Delete user falhou !');
+        }
+    }).catch(function(error){
+        signale.error('Delete user falhou ! - ' + error.message);
+    });
+
+  }
