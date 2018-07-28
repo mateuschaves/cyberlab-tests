d1b9ea546f31b97bca20b181df8b43a50db76cd6
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-27 21:20:46 -0300

    Múltiplos registros

--- src/Auth/RegisterTest.js
+++ src/Auth/RegisterTest.js
@@ -1,29 +1,32 @@
 const axios = require('axios');
 const signale = require('signale');
+const casual = require('casual');
+
+const QTN = 200;
     /*
         Teste register user
     */
-    async function register(){
-       axios.post('http://cyberlab-com.umbler.net/register', {
-          username: 'mateus122',
-          email: 'mateuss@hotmail.com.br',
-          password: '1234'
-      },
-      {
-          'Access-Control-Allow-Origin': "*"
-      }).then(async function(response){
-          console.log(response.status);
-          if(response.status == 201){
-              await signale.success('Registro bem sucedido !');
-              return true;
-          }else{
-              signale.error('Registro falhou !');
-              return false;
-          }
-      }).catch(function(error){
-          signale.error('Registro falhou ! - ' + error.message);
-          //console.log(error);
-          return false;
-      });
+    for(var i = 0; i < QTN; i++){
+        axios.post('http://cyberlab-com.umbler.net/register', {
+            username: casual.username,
+            email: casual.email,
+            password: casual.password
+        },
+        {
+            'Access-Control-Allow-Origin': "*"
+        }).then(async function(response){
+            console.log(response.status);
+            if(response.status == 201){
+                await signale.success('Registro bem sucedido !');
+                return true;
+            }else{
+                signale.error('Registro falhou !');
+                return false;
+            }
+        }).catch(function(error){
+            signale.error('Registro falhou ! - ' + error.message);
+            //console.log(error);
+            return false;
+        });
     }
-    register();
+    
