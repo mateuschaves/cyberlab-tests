03677a249dfdea7a2b7395096383ba61a8be035b
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-07-29 23:44:33 -0300

    múltiplos registros

--- src/Occurrence/CreateOccurrenceTest.js
+++ src/Occurrence/CreateOccurrenceTest.js
@@ -5,27 +5,30 @@ const token = require('../token');
 const casual = require('casual');
 
 
-const QTN = 1;
+const QTN = 10;
 
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
-const data = {
-    year: casual.year,
-    month: casual.month_number,
-    day: casual.day_of_month,
-    time: casual.time(format = 'HH:mm:ss'),
-    address: casual.address,
-    number: casual.integer(from = 1, to = 200),
-    neighborhood: casual.word,
-    city: casual.city,
-    state: casual.state
- }
+
+for(var i = 0; i < QTN; i++){
+    const data = {
+        year: casual.year,
+        month: casual.month_number,
+        day: casual.day_of_month,
+        time: casual.time(format = 'HH:mm:ss'),
+        address: casual.address,
+        number: casual.integer(from = 1, to = 200),
+        neighborhood: casual.word,
+        city: casual.city,
+        state: casual.state
+     }
     axios.post('http://cyberlab-com.umbler.net/occurrence/store',data,
-      {
-          'Access-Control-Allow-Origin': "*"
-      }).then(function(response){
-          if( response.status == 201 ){
-            signale.success('Create occurrence bem sucedido !');
-          }
-      }).catch(function(error){
-          signale.error('Create occurrence falhou ! - ' + error.response.data.message + ' ' + error.message);
-      });
+          {
+              'Access-Control-Allow-Origin': "*"
+          }).then(function(response){
+              if( response.status == 201 ){
+                signale.success('Create occurrence bem sucedido !');
+              }
+          }).catch(function(error){
+              signale.error('Create occurrence falhou ! - ' + error.response.data.message + ' ' + error.message);
+    });
+}
